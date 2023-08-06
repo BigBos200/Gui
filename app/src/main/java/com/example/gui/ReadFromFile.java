f8d5e0140eebbd2810d1e3ed4b89e2955b392e8a
Zeta <dziedzinaadam141@gmail.com>
2023-08-06 17:19:56 +0200

    Girafe android studio

--- app/src/main/java/com/example/gui/ReadFromFile.java
+++ app/src/main/java/com/example/gui/ReadFromFile.java
@@ -9,12 +9,8 @@ import com.example.gui.Focus.Efocus;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -26,7 +22,7 @@ public class ReadFromFile<F,U,E,V>{
     private Efocus eunit;
     private EditText editText;
     private TextView textView;
-
+    private String path = Environment.getExternalStorageDirectory().toString();
 
 
     public ReadFromFile(F efocus, U eunit, E editText, V textView) {
@@ -37,106 +33,73 @@ public class ReadFromFile<F,U,E,V>{
 
     }
 
-
-//    public ReadFromFile(EditText editText, TextView textView){
-//        this.editText = editText;
-//        this.textView = textView;
-//    }
-
-
     public void readFocus(){
-        readFromEfocus(efocus, eunit);
+        readFromEfocus(efocus, eunit, path);
     }
 
 
-    private void readFromEfocus(@NotNull Efocus efocus, Efocus eunit) {
-        String path = Environment.getExternalStorageDirectory().toString();
+    private void readFromEfocus(@NotNull Efocus efocus, Efocus eunit, String path) {
 
         switch (efocus){
             case FOCUS1:
                 switch (eunit){
-                    case UNIT1:
-                        break;
+                    case UNIT1 -> readFromFile(path, efocus, eunit);
+                    case UNIT2 -> readFromFile(path, efocus, eunit);
+                    case UNIT3 -> readFromFile(path, efocus, eunit);
+                    case UNIT4 -> readFromFile(path, efocus, eunit);
+                    case UNIT5 -> readFromFile(path, efocus, eunit);
+                    case UNIT6 -> readFromFile(path, efocus, eunit);
+                    case UNIT7 -> readFromFile(path, efocus, eunit);
+                    case UNIT8 -> readFromFile(path, efocus, eunit);
                 }
             break;
             case FOCUS2:
                 switch (eunit){
-                    case UNIT1:
-                        break;
+                    case UNIT1 -> readFromFile(path, efocus, eunit);
+                    case UNIT2 -> readFromFile(path, efocus, eunit);
+                    case UNIT3 -> readFromFile(path, efocus, eunit);
+                    case UNIT4 -> readFromFile(path, efocus, eunit);
+                    case UNIT5 -> readFromFile(path, efocus, eunit);
+                    case UNIT6 -> readFromFile(path, efocus, eunit);
+                    case UNIT7 -> readFromFile(path, efocus, eunit);
+                    case UNIT8 -> readFromFile(path, efocus, eunit);
                 }
             break;
             case FOCUS3:
                 switch (eunit){
-                    case UNIT1:
-                        break;
-                    case UNIT2:
-                        break;
-                    case UNIT3:
-                        break;
-                    case UNIT4:
-                        break;
-                    case UNIT5:
-                        break;
-                    case UNIT6:
-                        File file6 = new File(path + "/Download/Gui/Focus3/Unit6.txt");
-                        try {
-                            Scanner read = new Scanner(file6);
-                            while (read.hasNextLine()){
-                                words.put(read.nextLine(), read.nextLine());
-                            }
-                        } catch (FileNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                        for (Map.Entry<String, String> x : words.entrySet()){
-                            System.out.println(x.getKey() + " " +  x.getValue());
-                        }
-                        break;
-                    case UNIT7:
-                        break;
-                    case UNIT8:
-                        break;
+                    case UNIT1 -> readFromFile(path, efocus, eunit);
+                    case UNIT2 -> readFromFile(path, efocus, eunit);
+                    case UNIT3 -> readFromFile(path, efocus, eunit);
+                    case UNIT4 -> readFromFile(path, efocus, eunit);
+                    case UNIT5 -> readFromFile(path, efocus, eunit);
+                    case UNIT6 -> readFromFile(path, efocus, eunit);
+                    case UNIT7 -> readFromFile(path, efocus, eunit);
+                    case UNIT8 -> readFromFile(path, efocus, eunit);
                 }
                 break;
 
                 case FOCUS4:
                 switch (eunit){
-                    case UNIT1:
-                        break;
-                    case UNIT2:
-                        break;
-                    case UNIT3:
-                        break;
-                    case UNIT4:
-                        break;
-                    case UNIT5:
-                        break;
-                    case UNIT6:
-                        break;
-                    case UNIT7:
-                        break;
-                    case UNIT8:
-                        break;
+                    case UNIT1 -> readFromFile(path, efocus, eunit);
+                    case UNIT2 -> readFromFile(path, efocus, eunit);
+                    case UNIT3 -> readFromFile(path, efocus, eunit);
+                    case UNIT4 -> readFromFile(path, efocus, eunit);
+                    case UNIT5 -> readFromFile(path, efocus, eunit);
+                    case UNIT6 -> readFromFile(path, efocus, eunit);
+                    case UNIT7 -> readFromFile(path, efocus, eunit);
+                    case UNIT8 -> readFromFile(path, efocus, eunit);
                 }
                 break;
                 case FOCUS5:
                 switch (eunit){
-                    case UNIT1:
-                        break;
-                    case UNIT2:
-                        break;
-                    case UNIT3:
-                        break;
-                    case UNIT4:
-                        break;
-                    case UNIT5:
-                        break;
-                    case UNIT6:
-                        break;
-                    case UNIT7:
-                        break;
-                    case UNIT8:
-                        break;
+                    case UNIT1 -> readFromFile(path, efocus, eunit);
+                    case UNIT2 -> readFromFile(path, efocus, eunit);
+                    case UNIT3 -> readFromFile(path, efocus, eunit);
+                    case UNIT4 -> readFromFile(path, efocus, eunit);
+                    case UNIT5 -> readFromFile(path, efocus, eunit);
+                    case UNIT6 -> readFromFile(path, efocus, eunit);
+                    case UNIT7 -> readFromFile(path, efocus, eunit);
+                    case UNIT8 -> readFromFile(path, efocus, eunit);
                 }
                 break;
 
@@ -146,74 +109,23 @@ public class ReadFromFile<F,U,E,V>{
 
         }
 
-
-        if (efocus.equals(Efocus.FOCUS1)) {
-
-
-        } else if (efocus.equals(Efocus.FOCUS2)) {
-
-        } else if (efocus.equals(Efocus.FOCUS3)) {
-            if (eunit.equals(Efocus.UNIT1)){
-
-            }else if (eunit.equals(Efocus.UNIT2)){
-
-            }else if (eunit.equals(Efocus.UNIT3)){
-
-            }else if (eunit.equals(Efocus.UNIT4)){
-
-            }else if (eunit.equals(Efocus.UNIT5)){
-
-            }else if (eunit.equals(Efocus.UNIT6)){
-//                try {
-//                    File file6 = new File(path + "/Download/Gui/Focus3/Unit6.txt");
-//                    FileInputStream fileRead6 = new FileInputStream(file6);
-//                    BufferedReader reader6 = new BufferedReader(new InputStreamReader((fileRead6)));
-//                    while (reader6.readLine() != null){
-//                        words.put(reader6.readLine(), reader6.readLine());
-//                    }
-//                    reader6.close();
-//                    fileRead6.close();
-//
-//                    for (Map.Entry<String, String> x : words.entrySet()){
-//                        System.out.println(x.getKey() + " " + x.getValue());
-//                    }
-//
-//
-//                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-//                }
-
-//                File file6 = new File(path + "/Download/Gui/Focus3/Unit6.txt");
-//                try {
-//                    Scanner read = new Scanner(file6);
-//                    while (read.hasNextLine()){
-//                        words.put(read.nextLine(), read.nextLine());
-//                    }
-//                } catch (FileNotFoundException e) {
-//                    throw new RuntimeException(e);
-//                }
-//
-//                for (Map.Entry<String, String> x : words.entrySet()){
-//                    System.out.println(x.getKey() + " " +  x.getValue());
-//                }
-
-
-            }else if (eunit.equals(Efocus.UNIT7)){
-
-            }else if (eunit.equals(Efocus.UNIT8)){
-
+    private void readFromFile(String path, Efocus efocus, Efocus eunit){
+        File file6 = new File(path + "/Download/Gui/" + efocus.toString() + "/" + eunit.toString() + ".txt");
+        try {
+            Scanner read = new Scanner(file6);
+            while (read.hasNextLine()){
+                words.put(read.nextLine(), read.nextLine());
             }
-
-
-        } else if (efocus.equals(Efocus.FOCUS4)) {
-
-        } else if (efocus.equals(Efocus.FOCUS5)) {
-
-        } else if (efocus.equals(Efocus.SETTINGS)) {
-
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
+        for (Map.Entry<String, String> x : words.entrySet()){
+            System.out.println(x.getKey() + " " +  x.getValue());
+        }
     }
+
+
 
     public void read(){
         readFromHashMap(editText, textView);
@@ -221,25 +133,6 @@ public class ReadFromFile<F,U,E,V>{
 
 
     private void readFromHashMap(EditText inputText, TextView outputText){
-
-        inputText.findViewById(R.id.txtView6);
-        outputText.findViewById(R.id.editTxt6);
-
-
-//        switch (inputText.toString()){
-//            case "editTxt6":
-//                inputText.findViewById(R.id.txtView6);
-//                break;
-//
-//        }
-//
-//        switch (outputText.toString()){
-//            case "editTxt6":
-//                outputText.findViewById(R.id.editTxt6);
-//                break;
-//
-//        }
-
         for (Map.Entry<String, String> x : words.entrySet()){
             if (inputText.getText().toString().equals(x.getValue())){
                 outputText.setText(x.getKey());
